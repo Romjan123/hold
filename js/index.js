e04c26534b658092f04475d564a0c6f1001a765b
Romjan Bepari <62552583+Romjan123@users.noreply.github.com>
2022-09-28 14:42:57 +0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/*   =======================================typing================================*/
+var typed = new Typed(".typing",{
+    String:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+})
