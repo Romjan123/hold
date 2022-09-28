e840effa4d0ad2549cd4e269b67509587521a1bd
Romjan Bepari <62552583+Romjan123@users.noreply.github.com>
2022-09-28 14:39:59 +0400

    Delete Personal directory

--- Personal/js/style-switcher.js
+++
@@ -1,5 +0,0 @@
-/*==================toggle style switcher================*/
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click",() => {
-  document.querySelector(".style-switcher").classList.toggle("open");
-})
