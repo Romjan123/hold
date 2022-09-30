6f7bf346e08b40c8285c55b49486e43a79159251
Romjan Bepari <62552583+Romjan123@users.noreply.github.com>
2022-09-30 11:30:06 +0400

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-/*   =======================================typing================================*/
-var typed = new Typed(".typing",{
-    String:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-})
